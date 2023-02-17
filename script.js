29d791e446c3366471a324d4195283c230808e74
Sebastian Rekling <sebastianrekling@hotmail.com>
2023-02-17 10:22:27 +0100

    small changes

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 // Lav variablen "btn", der henviser til ".toggle-btn"
 const btn = document.querySelector(".toggle-btn");
-// Lav variablen "menu", der henviser til ".nav-links"
+// Lav variablen "menu", der henviser til ".toggle-menu"
 const menu = document.querySelector(".toggle-menu");
 
 // Lav funktionen "toggleMenu()" med følgende funktionalitet
 function toggleMenu() {
   // a) toggle klassen "shown" på menu vha. classList.toggle
 
-    menu.classList.toggle("shown");
-    btn.classList.toggle("pressed");
+  menu.classList.toggle("shown");
+  // btn.classList.toggle("pressed");
 
   // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
-    const menuShown = menu.classList.contains("shown");
+  const menuShown = menu.classList.contains("shown");
 
-  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if(menuShown)), og udskift teksten
-    if (menuShown) {
-    console.log(menuShown); // se i konsollen
-    // btn.textContent = "Luk"; //hvis menuShown er "true"
-    } else {
-    console.log(menuShown); // se i konsollen
-
-    // btn.textContent = "Menu"; // hvis menuShown er "false"
-    }
-  // "toggleMenu()" slutter her
+  console.log(menuShown); // se i konsollen true/false
 }
 // Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
 btn.addEventListener("click", toggleMenu);
